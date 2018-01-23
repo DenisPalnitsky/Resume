64a9b8c6a11cd060a7a3aed5d09513c7cc1d5b45
Denis Palnitsky <palnitsky@gmail.com>
2018-01-23 09:32:32 +0100

    Initial commit

---
+++ GoogleDocPublish/GoogleDocPublish/Controllers/GoogleDocHtmlController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GoogleDocPublish.Controllers
+{
+    [Route("api/[controller]")]
+    public class GoogleDocHtmlController : Controller
+    {
+        [HttpGet]
+        public async Task<string> Get()
+        {
+            var client = new HttpClient();
+            return await client.GetStringAsync("https://docs.google.com/document/d/e/2PACX-1vRYL5Si8vWz85AGPAMye8JKy71Ob3AYxMz-dxl0S_BLQWQMqbRnIouQYN8IfOH9Lae3vt3dn6YeO79g/pub");
+        }
+
+
+    }
+}
