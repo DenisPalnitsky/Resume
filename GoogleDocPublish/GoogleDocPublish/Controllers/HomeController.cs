0b1a6167b7a30f8cba3603a2e17b9d169ca3b37c
Denis Palnitsky <palnitsky@gmail.com>
2018-01-23 13:32:48 +0100

    Use ASP.net to generate CV page

---
+++ GoogleDocPublish/GoogleDocPublish/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using GoogleDocPublish.Models;
+using System.Net.Http;
+using HtmlAgilityPack;
+
+namespace GoogleDocPublish.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var client = new HttpClient();
+            var res =  await client.GetStringAsync("https://docs.google.com/document/d/e/2PACX-1vRYL5Si8vWz85AGPAMye8JKy71Ob3AYxMz-dxl0S_BLQWQMqbRnIouQYN8IfOH9Lae3vt3dn6YeO79g/pub");
+            var document = new HtmlDocument();
+            document.LoadHtml(res);
+            var nodes = document.DocumentNode.SelectSingleNode("//div[@id='contents']");
+
+            ViewData["Content"] = nodes.OuterHtml;
+            return View();
+        }
+  
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
--- GoogleDocPublish/GoogleDocPublish/Startup.cs
+++ GoogleDocPublish/GoogleDocPublish/Startup.cs
@@ -6,8 +6,6 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace GoogleDocPublish
 {
@@ -31,11 +29,22 @@ namespace GoogleDocPublish
         {
             if (env.IsDevelopment())
             {
+                app.UseBrowserLink();
                 app.UseDeveloperExceptionPage();
             }
-            app.UseDefaultFiles();
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
             app.UseStaticFiles();
-            app.UseMvc();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
         }
     }
 }
