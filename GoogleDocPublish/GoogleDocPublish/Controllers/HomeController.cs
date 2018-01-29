1daab6c5f6acc878b4c9c7d6f9905d892635af92
Denis_Palnitsky <dpalnitsky@corelogic.com>
2018-01-29 17:42:17 +0100

    Used model to pass data to view

--- GoogleDocPublish/GoogleDocPublish/Controllers/HomeController.cs
+++ GoogleDocPublish/GoogleDocPublish/Controllers/HomeController.cs
@@ -12,16 +12,18 @@ namespace GoogleDocPublish.Controllers
 {
     public class HomeController : Controller
     {
+        private const string DocId = "1Rzf7T8J2RKAg_DHUu4M6Jyx4igzBQbIhUQ8f5ZFkJks";
+        private const string DocHtmlRequestUri = "https://docs.google.com/document/d/e/2PACX-1vRYL5Si8vWz85AGPAMye8JKy71Ob3AYxMz-dxl0S_BLQWQMqbRnIouQYN8IfOH9Lae3vt3dn6YeO79g/pub";
+
         public async Task<IActionResult> Index()
         {
             var client = new HttpClient();
-            var res =  await client.GetStringAsync("https://docs.google.com/document/d/e/2PACX-1vRYL5Si8vWz85AGPAMye8JKy71Ob3AYxMz-dxl0S_BLQWQMqbRnIouQYN8IfOH9Lae3vt3dn6YeO79g/pub");
+            var res =  await client.GetStringAsync(DocHtmlRequestUri);
             var document = new HtmlDocument();
             document.LoadHtml(res);
             var nodes = document.DocumentNode.SelectSingleNode("//div[@id='contents']");
-
-            ViewData["Content"] = nodes.OuterHtml;
-            return View();
+            
+            return View( new ResumeViewModel(DocId, nodes.OuterHtml));
         }
   
         public IActionResult Error()
---
+++ GoogleDocPublish/GoogleDocPublish/Models/ResumeViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoogleDocPublish.Models
+{
+    public class ResumeViewModel
+    {
+        enum Formats
+        {
+            pdf,
+            doc
+        }
+        
+        const string URL_PATTERN = "https://docs.google.com/document/export?format={0}&id={1}";
+
+        string _docId;
+
+        public string PdfDownloadLink => BuildUrl(Formats.pdf, _docId);
+
+        public string WordDownloadLink => BuildUrl(Formats.doc, _docId);
+
+        static string BuildUrl(Formats format, string docId)
+        {
+            return string.Format(URL_PATTERN, format, docId);
+        }
+
+        public string ResumeHtml { get; private set; }
+
+        public ResumeViewModel (string docId, string html)
+        {
+            ResumeHtml = html;
+            this._docId = docId;
+
+        }
+
+    }
+}
